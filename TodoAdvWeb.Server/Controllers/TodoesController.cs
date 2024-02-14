c1c50f508afb69a44cb0ffa1a74c13a8df573629
Jesse Harlan <jesse.harlan@centralia.edu>
2024-02-14 10:53:31 -0800

    added addnewitem to app.component Added space to a thing

--- TodoAdvWeb.Server/Controllers/TodoesController.cs
+++ TodoAdvWeb.Server/Controllers/TodoesController.cs
@@ -38,6 +38,7 @@ namespace TodoAdvWeb.Server.Controllers
             if (todo == null)
             {
                 return NotFound();
+
             }
 
             return todo;
--- todoadvweb.client/src/app/app.component.ts
+++ todoadvweb.client/src/app/app.component.ts
@@ -24,5 +24,19 @@ export class AppComponent implements OnInit {
     });
   }
 
+  addNewTodo() {
+    let item: Todo = {
+        todoId: 0,
+        description: 'Put new items into the list',
+        ownerId: '88290c71-694e-443d-b46f-dfebc19e4393',
+        completed: false,
+        todoUserId: ''
+    }
+
+    this.http.post<Todo>("/api/Todoes", item).subscribe(data => {
+      this.todoList.push(data);
+    });
+  }
+
   title = 'todoadvweb.client';
 }
